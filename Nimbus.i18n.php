9f4c28b8817912f07494c8cdb347ecfeea9c973d
Jack Phoenix <ashley@users.mediawiki.org>
2011-09-06 18:09:04 +0000

    Nimbus -- the skin used on Halopedia (http://www.halopedian.com/). Tested against MediaWiki 1.18, requires ResourceLoader. TODO: Nimbus_IE.css should be loaded for Internet Explorers, but currently it isn't.

---
+++ Nimbus.i18n.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Internationalization file for the Nimbus skin.
+ *
+ * @file
+ */
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'morewikis' => '', // format for this message is *Page name/Interwiki link/URL|Text to show
+	'nimbus-advertise' => 'Advertise',
+	'nimbus-advertise-url' => '', // set this to an URL if you want an "Advertise" link to show up in the footer
+	'nimbus-contribute' => 'Contribute',
+	'nimbus-didyouknow' => 'Did You Know',
+	'nimbus-featuredimage' => 'Featured Image',
+	'nimbus-featureduser' => 'Featured User',
+	'nimbus-login' => 'Login',
+	'nimbus-logout' => 'Log Out?',
+	'nimbus-more-actions' => 'More Actions',
+	'nimbus-more-wikis' => 'More Wikis',
+	'nimbus-pages-can-be-edited' => "{{SITENAME}}'s pages can be edited. <br />
+Is this page incomplete?  Is there anything wrong?<br />
+<b>Change it!</b>",
+	'nimbus-profile' => 'Profile',
+	'nimbus-recent-contributors' => 'Recent contributors to this page',
+	'nimbus-recent-contributors-info' => 'The following people recently contributed to this article.',
+	'nimbus-search' => 'enter search',
+	'nimbus-signup' => 'Sign Up',
+	'nimbus-welcome' => 'Welcome $1', // <b></b> tags are passed as $1 along with the actual username
+);
+
+/** Finnish (Suomi)
+ * @author Jack Phoenix <jack@countervandalism.net>
+ */
+$messages['fi'] = array(
+	'nimbus-advertise' => 'Mainosta',
+	'nimbus-contribute' => 'Muokkaa',
+	'nimbus-didyouknow' => 'Tiesitkö, että...',
+	'nimbus-featuredimage' => 'Suositeltu kuva',
+	'nimbus-featureduser' => 'Suositeltu käyttäjä',
+	'nimbus-login' => 'Kirjaudu sisään',
+	'nimbus-logout' => 'Kirjaudu ulos?',
+	'nimbus-more-actions' => 'Lisää toimintoja',
+	'nimbus-more-wikis' => 'Lisää wikejä',
+	'nimbus-pages-can-be-edited' => '{{GRAMMAR:genitive|{{SITENAME}}}} sivuja voi muokata.<br />
+Onko tämä sivu puutteellinen? Onko siinä jotakin vikaa?<br />
+<b>Muuta sitä!</b>',
+	'nimbus-profile' => 'Profiili',
+	'nimbus-recent-contributors' => 'Tämän sivun tuoreimmat muokkaajat',
+	'nimbus-recent-contributors-info' => 'Seuraavat henkilöt ovat muokanneet tätä artikkelia äskettäin.',
+	'nimbus-search' => 'anna hakusana',
+	'nimbus-signup' => 'Rekisteröidy',
+	'nimbus-welcome' => 'Tervetuloa $1',
+);
+
+/** French (Français)
+ * @author Jack Phoenix <jack@countervandalism.net>
+ */
+$messages['fr'] = array(
+	'nimbus-welcome' => 'Bienvenue $1',
+);
+
+/** Dutch (Nederlands)
+ * @author Mitchel Corstjens
+ */
+$messages['nl'] = array(
+	'nimbus-login' => 'Inloggen',
+	'nimbus-logout' => 'Uitloggen?',
+	'nimbus-welcome' => 'Welkom $1',
+);
+
+/** Polish (Polski)
+ * @author Misiek95
+ */
+$messages['pl'] = array(
+	'nimbus-advertise' => 'Reklamy',
+	'nimbus-contribute' => 'Wnieś swój wkład',
+	'nimbus-didyouknow' => 'Czy wiedziałeś?',
+	'nimbus-featuredimage' => 'Promowany obrazek',
+	'nimbus-featureduser' => 'Promowany użytkownik',
+	'nimbus-login' => 'Zaloguj się',
+	'nimbus-logout' => 'Wylogować?',
+	'nimbus-more-actions' => 'Inne akcje...',
+	'nimbus-more-wikis' => 'Więcej wiki...',
+	'nimbus-pages-can-be-edited' => '{{SITENAME}} jest wiki, więc strony mogą być edytowane przez każdego.<br />
+Strona jest niekompletna? Znalazłeś błąd?<br />
+<b>Zmień to!</b>',
+	'nimbus-profile' => 'Profil',
+	'nimbus-recent-contributors' => 'Ostatni edytorzy tej strony',
+	'nimbus-recent-contributors-info' => 'Te osoby ostatnio edytowały ten artykuł.',
+	'nimbus-search' => 'Wpisz szukane wyrażenie',
+	'nimbus-signup' => 'Zarejestruj się',
+	'nimbus-welcome' => 'Witaj, $1',
+);
