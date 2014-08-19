9f7b1a49f7ce6a33d0728b92c6304f07b39c12b8
Jack Phoenix <jack@countervandalism.net>
2014-08-19 21:02:26 +0300

    Convert to JSON-based i18n, bump version number + copyright year
    Change-Id: I0960adc94d3222aee7af1335317997dd5b5456cc

--- Nimbus.php
+++ Nimbus.php
@@ -8,9 +8,9 @@
  * @author David Pean <david.pean@gmail.com>
  * @author Inez Korczyński <korczynski@gmail.com>
  * @author Jack Phoenix <jack@countervandalism.net>
- * @copyright Copyright © 2008-2013 Aaron Wright, David Pean, Inez Korczyński, Jack Phoenix
+ * @copyright Copyright © 2008-2014 Aaron Wright, David Pean, Inez Korczyński, Jack Phoenix
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @date July 2, 2013
+ * @date 19 August 2014
  *
  * To install place the Nimbus folder (the folder containing this file!) into
  * skins/ and add this line to your wiki's LocalSettings.php:
@@ -25,25 +25,21 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['skin'][] = array(
 	'path' => __FILE__,
 	'name' => 'Nimbus',
-	'version' => '2.5',
+	'version' => '3.0',
 	'author' => array( 'Aaron Wright', 'David Pean', 'Inez Korczyński', 'Jack Phoenix' ),
 	'descriptionmsg' => 'nimbus-desc',
-	'url' => 'http://www.mediawiki.org/wiki/Nimbus_skin',
+	'url' => 'https://www.mediawiki.org/wiki/Skin:Nimbus',
 );
-
-// Autoload the skin class, make it a valid skin, set up i18n, set up CSS & JS
-// (via ResourceLoader)
-$skinID = basename( dirname( __FILE__ ) );
-$dir = dirname( __FILE__ ) . '/';
 
 // The first instance must be strtolower()ed so that useskin=nimbus works and
 // so that it does *not* force an initial capital (i.e. we do NOT want
 // useskin=Nimbus) and the second instance is used to determine the name of
 // *this* file.
-$wgValidSkinNames[strtolower( $skinID )] = 'Nimbus';
+$wgValidSkinNames['nimbus'] = 'Nimbus';
 
-$wgAutoloadClasses['SkinNimbus'] = $dir . 'Nimbus.skin.php';
-$wgExtensionMessagesFiles['SkinNimbus'] = $dir . 'Nimbus.i18n.php';
+// Autoload the skin class, set up i18n, set up CSS & JS (via ResourceLoader)
+$wgAutoloadClasses['SkinNimbus'] = __DIR__ . '/Nimbus.skin.php';
+$wgMessagesDirs['MonoBook'] = __DIR__ . '/i18n';
 $wgResourceModules['skins.nimbus'] = array(
 	'styles' => array( 'skins/Nimbus/nimbus/Nimbus.css' => array( 'media' => 'screen' ) ),
 	'scripts' => 'skins/Nimbus/nimbus/Menu.js',
